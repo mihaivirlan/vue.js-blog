ae1b81b8ca1dfb4eb4be474aeee01e839e2deceb
mihail.virlan <mihail.virlan@esolutions.ro>
2020-04-10 09:30:57 +0300

    initial commit

---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="about-page">
+        <h1>This is my absolutely fantastic "About Me" Page!</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing </p>
+        <p>It is a long established fact that a reader will be distracted</p>
+        <p>Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+        <p>There are many variations of passages of Lorem Ipsum available</p>
+    </div>
+</template>
+
+<style scoped>
+.about-page {
+    text-align: center;
+    padding: 30px;
+}
+</style>
